0540c32b24ddc7c9add0879d01bbb617df3df2e2
Mike <nhj25252@gmail.com>
2023-04-27 21:49:32 +0900

    calculate factorial

--- script.js
+++ script.js
@@ -13,12 +13,25 @@
 // 2. Power function
 // takes in a base and an exponent. If the exponent is 0, return 1.
 
-function power(base, exponent) {
-  if (exponent == 0) {
+// function power(base, exponent) {
+//   if (exponent == 0) {
+//     return 1;
+//   } else {
+//     return base * power(base, exponent - 1);
+//   }
+// }
+
+// console.log(power(2, 4));
+
+// 3. calculate factorial.
+// write a function that returns the factorial of a number.
+
+function factorial(n) {
+  if (n == 1) {
     return 1;
   } else {
-    return base * power(base, exponent - 1);
+    return n * factorial(n - 1);
   }
 }
 
-console.log(power(2, 4));
+console.log(factorial(5));
