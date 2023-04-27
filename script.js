5b53840ca8f7ad79266c948f815b5d73023c4915
Mike <nhj25252@gmail.com>
2023-04-27 21:45:29 +0900

    2. power function

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-// 1. Sum all numbers
+// // 1. Sum all numbers
 
-function sumRange(n) {
-  if (n == 1) {
+// function sumRange(n) {
+//   if (n == 1) {
+//     return 1;
+//   } else {
+//     return n + sumRange(n - 1);
+//   }
+// }
+
+// console.log(sumRange(6));
+
+// 2. Power function
+// takes in a base and an exponent. If the exponent is 0, return 1.
+
+function power(base, exponent) {
+  if (exponent == 0) {
     return 1;
   } else {
-    return n + sumRange(n - 1);
+    return base * power(base, exponent - 1);
   }
 }
 
-console.log(sumRange(6));
+console.log(power(2, 4));
