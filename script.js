941d936aaee4fcdbcd052333cd55e15098f53d7b
Mike <nhj25252@gmail.com>
2023-04-28 23:38:09 +0900

    fibonacci sequence

--- script.js
+++ script.js
@@ -26,12 +26,31 @@
 // 3. calculate factorial.
 // write a function that returns the factorial of a number.
 
-function factorial(n) {
+// function factorial(n) {
+//   if (n == 1) {
+//     return 1;
+//   } else {
+//     return n * factorial(n - 1);
+//   }
+// }
+
+// console.log(factorial(5));
+
+// 4. fibonacci sequence
+
+let array = [];
+
+function fibsRec(n) {
   if (n == 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
+    array = [0];
+    return array;
+  } else if (n == 2) {
+    array = [0, 1];
+    return array;
+  } else if (n >= 3) {
+    const newNum = fibsRec(n - 1).at(-2) + fibsRec(n - 1).at(-1);
+    fibsRec(n - 1).push(newNum);
+    return array;
   }
 }
-
-console.log(factorial(5));
+console.log(fibsRec(10));
