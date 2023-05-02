fc931ce1ac44663f1b4a4489903bff81439ef35f
Mike <nhj25252@gmail.com>
2023-05-02 18:48:26 +0900

    check all values in an array

--- script.js
+++ script.js
@@ -38,19 +38,34 @@
 
 // 4. fibonacci sequence
 
-let array = [];
+// let array = [];
 
-function fibsRec(n) {
-  if (n == 1) {
-    array = [0];
-    return array;
-  } else if (n == 2) {
-    array = [0, 1];
-    return array;
-  } else if (n >= 3) {
-    const newNum = fibsRec(n - 1).at(-2) + fibsRec(n - 1).at(-1);
-    fibsRec(n - 1).push(newNum);
-    return array;
-  }
+// function fibsRec(n) {
+//   if (n == 1) {
+//     array = [0];
+//     return array;
+//   } else if (n == 2) {
+//     array = [0, 1];
+//     return array;
+//   } else if (n >= 3) {
+//     const newNum = fibsRec(n - 1).at(-2) + fibsRec(n - 1).at(-1);
+//     fibsRec(n - 1).push(newNum);
+//     return array;
+//   }
+// }
+// console.log(fibsRec(10));
+
+// 5. check all values in an array
+// a function called all. accepts an array and a callback
+// function all returns true, if every value in the array returns true
+
+function all(array, callback) {
+  if (array.every(callback)) return true;
+  else return false;
 }
-console.log(fibsRec(10));
+
+var allAreLessThanSeven = all([1, 2, 9], function (num) {
+  return num < 10;
+});
+
+console.log(allAreLessThanSeven); // false
